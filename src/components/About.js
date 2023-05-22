d7980eaa04411aff8b938861d927a00a23b2663a
Harpreet Singh <Harpreets1201@gmail.com>
2023-05-22 04:50:34 -0400

    WIP

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About(props) {
+  function BioTest(props) {
+    if (!props.bio || props.bio === "") {
+      return null;
+    } else {
+      return <p>{props.bio}</p>;
+    }
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {BioTest(props)}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Links() {
+  return (
+    <h3>Links</h3>
+  );
+}
+
+export default Links;
